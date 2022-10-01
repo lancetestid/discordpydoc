19cd3a9a1c7d8f9bf215be1f52bf07fa7ed19a73
cnlancehu <2124757129@qq.com>
2022-10-01 09:32:22 +0800

    上传缓存

--- docs/.vuepress/.temp/pages/contribute.html.js
+++ docs/.vuepress/.temp/pages/contribute.html.js
@@ -1,1 +1,14 @@
 export const data = JSON.parse("{\"key\":\"v-39072b1a\",\"path\":\"/contribute.html\",\"title\":\"贡献指南\",\"lang\":\"zh-CN\",\"frontmatter\":{\"lang\":\"zh-CN\",\"title\":\"贡献指南\",\"sidebar\":\"贡献指南\"},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"文章准则\",\"slug\":\"文章准则\",\"link\":\"#文章准则\",\"children\":[]},{\"level\":2,\"title\":\"编辑技巧\",\"slug\":\"编辑技巧\",\"link\":\"#编辑技巧\",\"children\":[{\"level\":3,\"title\":\"FrontMatter\",\"slug\":\"frontmatter\",\"link\":\"#frontmatter\",\"children\":[]},{\"level\":3,\"title\":\"CodeGroup\",\"slug\":\"codegroup\",\"link\":\"#codegroup\",\"children\":[]},{\"level\":3,\"title\":\"自定义容器\",\"slug\":\"自定义容器\",\"link\":\"#自定义容器\",\"children\":[]}]}],\"git\":{\"updatedTime\":1664030858000,\"contributors\":[{\"name\":\"cnlancehu\",\"email\":\"2124757129@qq.com\",\"commits\":1}]},\"filePathRelative\":\"contribute.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
